a1455eb27c4addbe08f3e0054bfb344457623a20
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2025-07-30 13:54:18 +0900

    Introduce react-hook-form

--- ui/pages/Chat/Chat.tsx
+++ ui/pages/Chat/Chat.tsx
@@ -3,25 +3,29 @@ import { useSpeak } from "../../api/hooks.ts";
 import { Textarea } from "./components/Textarea.tsx";
 import { IconButton } from "./components/IconButton.tsx";
 import { PaperPlaneIcon } from "@radix-ui/react-icons";
+import { useForm } from "react-hook-form";
+
+type ChatForm = {
+  text: string;
+}
 
 export const Chat = () => {
   const { mutate: speak } = useSpeak();
+  const { formState: { isValid }, handleSubmit, register, reset } = useForm<ChatForm>()
+  const onSubmit = (data: ChatForm) => {
+    speak(data.text);
+    reset();
+  }
 
-
-  return <Form.Root onSubmit={(e) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const text = formData.get("text") as string;
-    speak(text);
-  }}>
+  return <Form.Root onSubmit={handleSubmit(onSubmit)}>
     <Form.Field name="text">
       <Form.Label>文章</Form.Label>
       <Form.Control asChild>
-        <Textarea />
+        <Textarea {...register('text', { required: true })} />
       </Form.Control>
     </Form.Field>
     <Form.Submit asChild>
-      <IconButton type="submit" label="送信">
+      <IconButton type="submit" label="送信" disabled={!isValid}>
         <PaperPlaneIcon />
       </IconButton>
     </Form.Submit>
