fcb80bf255146903c7beaba06063083b561a65f0
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2025-07-30 14:15:50 +0900

    Add reset CSS

---
+++ ui/reset.css.ts
@@ -0,0 +1,12 @@
+import { globalStyle } from "@vanilla-extract/css";
+
+globalStyle("html, body", {
+  margin: 0,
+  padding: 0,
+  boxSizing: "border-box",
+  fontFamily: "system-ui, sans-serif",
+  fontSize: "16px",
+  lineHeight: "1.5",
+  color: "#333",
+  backgroundColor: "#fff",
+})
